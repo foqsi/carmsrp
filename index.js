e0a9790b186d979c76b2e7a58503e6066cbe4014
Edward <81455653+foqsi@users.noreply.github.com>
2022-11-04 09:54:58 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,15 @@
+const menu = document.getElementById("menu");
+
+Array.from(document.getElementsByClassName("menu-item"))
+    .forEach((item, index) => {
+        item.onmouseover = () => {
+            menu.dataset.activeIndex = index;
+        }
+    });
+
+Array.from(document.getElementsByClassName("menu-item"))
+    .forEach((item, index) => {
+        item.onclick = () => {
+            menu.dataset.activeIndex = index;
+        }
+    });
