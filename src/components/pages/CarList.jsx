951dd88bb1f527287899c0c9f583ae5250c66ab7
Edward Davis <foqsi03@gmail.com>
2023-09-16 19:26:45 -0500

    changed name to CarList

---
+++ src/components/pages/CarList.jsx
@@ -0,0 +1,51 @@
+const CarList = {
+    'Acura': { 'Work in progress': {} },
+    'Alfa Romeo': { 'Work in progress': {} },
+    'Aston Martin': { 'Work in progress': {} },
+    'Audi': { 'Work in progress': {} },
+    'BMW': { 'Work in progress': {} },
+    'Buick': { 'Work in progress': {} },
+    'Cadillac': { 'Work in progress': {} },
+    'Chevrolet': { 'Work in progress': {} },
+    'Chrysler': { 'Work in progress': {} },
+    'Dodge': { 'Work in progress': {} },
+    'Ford': { 'Work in progress': {} },
+    'Genesis': { 'Work in progress': {} },
+    'GMC': { 'Work in progress': {} },
+    'Honda': { 'Work in progress': {} },
+    'Hyundai': { 'Work in progress': {} },
+    'Infiniti': { 'Work in progress': {} },
+    'Jaguar': { 'Work in progress': {} },
+    'Jeep': { 'Work in progress': {} },
+    'Kia': { 'Work in progress': {} },
+    'Lexus': { 'Work in progress': {} },
+    'Lincoln': { 'Work in progress': {} },
+    'Mazda': { 'Work in progress': {} },
+    'Mercedes Benz': { 'Work in progress': {} },
+    'Mercury': { 'Work in progress': {} },
+    'Mini': { 'Work in progress': {} },
+    'Mitsubishi': { 'Work in progress': {} },
+    'Nissan': { 'Work in progress': {} },
+    'Pontiac': { 'Work in progress': {} },
+    'RAM': { 'Work in progress': {} },
+    'Subaru': { 'Work in progress': {} },
+    'Tesla': { 'Work in progress': {} },
+    'Toyota': {
+        'Camry': {
+            '1990 - 2000': {
+                'years': [1990, 1991, 1992, 1993, 1994, 1995, 1996, 1997, 1998, 1999, 2000],
+                'trims': ['LE', 'SE']
+            },
+            '2001 - 2020': {
+                'years': [2001, 2002, 2003, 2004, 2005, 2006, 2007, 2008, 2009, 2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019, 2020],
+                'trims': ['LE', 'SE', 'XLE']
+            },
+        },
+        'Corolla': {},
+        'C-HR': {}
+    },
+    'Volvo': { 'Work in progress': {} },
+    'Volkswagen': { 'Work in progress': {} },
+};
+
+export default CarList;
