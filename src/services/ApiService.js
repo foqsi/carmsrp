36d6f261f8c188083378b05bfb4d170e00c72ee5
Edward Davis <foqsi03@gmail.com>
2023-10-07 21:14:18 -0500

    commented colors / not using yet

--- src/services/ApiService.js
+++ src/services/ApiService.js
@@ -59,17 +59,39 @@ export const fetchTrimDetails = async (selectedMake, selectedModel, selectedYear
     try {
         const response = await fetch(`${BASE_URL}/api/trims?direction=asc&sort=id&year=${selectedYear}&model=${selectedModel}&trim=${selectedTrim}&verbose=yes&make=${selectedMake}`, { headers });
         if (!response.ok) throw new Error('Not successful');
+
         const data = await response.json();
-        return data;
+
+        const uniqueTrimDetail = new Set();
+        const uniqueIds = new Set();
+        const newTrimDetailToIdMap = {};
+
+        data.data.forEach(item => {
+            uniqueTrimDetail.add(item.description);
+            uniqueIds.add(item.id);
+            newTrimDetailToIdMap[item.description] = item.id;
+        });
+
+        const uniqueTrimDetailArray = Array.from(uniqueTrimDetail);
+        const uniqueIdsArray = Array.from(uniqueIds);
+
+        return {
+            trimDetails: uniqueTrimDetailArray,
+            selectedIds: uniqueIdsArray,
+            trimDetailToIdMap: newTrimDetailToIdMap,
+            raw: data.data
+        };
+
     } catch (error) {
         console.error('An error occurred:', error);
         throw error;
     }
 }
 
-export const fetchVehicleInformation = async (selectedIds) => {
+
+export const fetchVehicleInformation = async (selectedId) => {
     try {
-        const response = await fetch(`${BASE_URL}/api/trims/${selectedIds}`, { headers });
+        const response = await fetch(`${BASE_URL}/api/trims/${selectedId}`, { headers });
         if (!response.ok) throw new Error('Not successful');
         const data = await response.json();
         return data;
@@ -79,26 +101,26 @@ export const fetchVehicleInformation = async (selectedIds) => {
     }
 }
 
-export const fetchExteriorColors = async (selectedIds) => {
-    try {
-        const response = await fetch(`${BASE_URL}/api/trims/${selectedIds}/colors/exterior`, { headers });
-        if (!response.ok) throw new Error('Not successful');
-        const data = await response.json();
-        return data;
-    } catch (error) {
-        console.error('An error occurred:', error);
-        throw error;
-    }
-}
+// export const fetchExteriorColors = async (selectedIds) => {
+//     try {
+//         const response = await fetch(`${BASE_URL}/api/trims/${selectedIds}/colors/exterior`, { headers });
+//         if (!response.ok) throw new Error('Not successful');
+//         const data = await response.json();
+//         return data;
+//     } catch (error) {
+//         console.error('An error occurred:', error);
+//         throw error;
+//     }
+// }
 
-export const fetchInteriorColors = async (selectedIds) => {
-    try {
-        const response = await fetch(`${BASE_URL}/api/trims/${selectedIds}/colors/interior`, { headers });
-        if (!response.ok) throw new Error('Not successful');
-        const data = await response.json();
-        return data;
-    } catch (error) {
-        console.error('An error occurred:', error);
-        throw error;
-    }
-}
+// export const fetchInteriorColors = async (selectedIds) => {
+//     try {
+//         const response = await fetch(`${BASE_URL}/api/trims/${selectedIds}/colors/interior`, { headers });
+//         if (!response.ok) throw new Error('Not successful');
+//         const data = await response.json();
+//         return data;
+//     } catch (error) {
+//         console.error('An error occurred:', error);
+//         throw error;
+//     }
+// }
